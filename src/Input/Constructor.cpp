f53807e11af255a99ea93e9d4ccf59b8c9287afd
LoveL05 <lovelinde05@gmail.com>
2022-07-11 16:36:56 +0200

    Create Constructor.cpp

---
+++ src/Input/Constructor.cpp
@@ -0,0 +1,5 @@
+#include <Input.h>
+
+std::set<int> InputSystem::ButtonsPressed = std::set<int>();
+std::set<int> InputSystem::ButtonsJustPressed = std::set<int>();
+std::set<int> InputSystem::ButtonsJustReleased = std::set<int>();
