b3350e25b4a419d029d39c8b0b6c662b88a87de7
LoveL05 <lovelinde05@gmail.com>
2022-07-11 16:36:43 +0200

    Added Input logic

--- src/Engine/Event.cpp
+++ src/Engine/Event.cpp
@@ -1,11 +1,19 @@
 #include <Engine.h>
 
 void Engine::Event(SDL_Event* Event) {
+    Input.ButtonsJustPressed.clear();
+    Input.ButtonsJustReleased.clear();
     while (SDL_PollEvent(Event)) {
         switch (Event->type)
         {
         case SDL_QUIT:
             Stop();
+            break;
+        case SDL_KEYDOWN:
+            Input.ButtonPressed(Event->key.keysym.sym);
+            break;
+        case SDL_KEYUP:
+            Input.ButtonReleased(Event->key.keysym.sym);
             break;
         default:
             break;
