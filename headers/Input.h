e71e2e36a8fbfd072387a7a750f760d15db2e85f
LoveL05 <lovelinde05@gmail.com>
2022-07-11 16:34:37 +0200

    Declared input functions and variables

---
+++ headers/Input.h
@@ -0,0 +1,20 @@
+#ifndef FED070C0_B471_48FE_9DB4_C26DE47A4B58
+#define FED070C0_B471_48FE_9DB4_C26DE47A4B58
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <Vectors.h>
+#include <set>
+
+class InputSystem {
+    public:
+        static std::set<int> ButtonsPressed, ButtonsJustPressed, ButtonsJustReleased;
+        void ButtonPressed(int button);
+        void ButtonReleased(int button);
+
+        bool GetButtonUp(const int button);
+        bool GetButtonDown(const int button);
+        bool GetButtonJustPressed(const int button);
+};
+
+#endif /* FED070C0_B471_48FE_9DB4_C26DE47A4B58 */
